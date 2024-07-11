f98464d45a31aa51edef14e7f9cb75bcfbd44dfd
Wai Yan Lin <75665112+William12445@users.noreply.github.com>
2024-07-11 09:08:09 +0900

    Add files via upload

---
+++ ggg.py
@@ -0,0 +1,25 @@
+﻿import numpy as np
+# r =np.array( [[1, 1, 1, 1],
+#     [1, 0, 0, 0],
+#     [1, 0, 0, 0],
+#     [1, 0, 0, 0]])
+# for i in range(1,4):
+#     for j in range(1,4):
+        
+#         r[i,j]=r[i-1,j]+r[i,j-1]
+# print(r)
+
+# num=np.array([0,0,0,0,0,0,0,0,0,0])
+# num[1]=1
+# print(num)
+# for i in range(2,len(num)):
+#     print(i)
+#     # num[i-1]=num[i]+num[i-1]
+#     # num[i-1]=num[i-1]+num[i-1]
+#     # num[i]=num[i]+num[i-2]
+#     num[i]=num[i-1]+num[i-2]
+#     print(num)
+
+a=[]
+
+print(a)
---
+++ model_data/main.py
@@ -0,0 +1,17 @@
+﻿from Dector import *
+import os
+
+def main():
+    videoPath = 0  # If you want to use a webcam, otherwise provide the path to the video file
+
+    base_dir = 'd:/230073/real_time_object_detection_cpu-main-20240607T015456Z-001/real_time_object_detection_cpu-main'
+
+    configPath = os.path.join(base_dir, "model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
+    modelPath = os.path.join(base_dir, "model_data", "frozen_inference_graph.pb")
+    classesPath = os.path.join(base_dir, "model_data", "coco.names")
+    
+    detector = Dector(videoPath, configPath, modelPath, classesPath)
+    detector.onVideo()
+
+if __name__ == "__main__":
+    main()
